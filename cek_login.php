541eb44ea0fa4c55b4fbba7a12931ae0439afe58
agil <agilabdulhadi@gmail.com>
2022-12-14 23:18:15 +0700

    [refac_login] refactoring style login

--- cek_login.php
+++ cek_login.php
@@ -3,8 +3,7 @@
     include 'koneksi.php';
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $data = mysqli_query($koneksi,"select * from admin where
-    username='$username' and password='$password'");
+    $data = mysqli_query($koneksi,"select * from admin where username='$username' and password='$password'");
     $cek = mysqli_num_rows($data);
     if($cek > 0){
         $_SESSION['username'] = $username;
--- index.php
+++ index.php
@@ -1,47 +1,121 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Membuat Login Dengan PHP </title>
-    </head>
-    <body>
-        <h2>Login</h2>
-        <br />
-        <?php
+<head>
+	<title>LOGIN</title>
+    <style>
+        body {
+            background: #1690A7;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            flex-direction: column;
+        }
+
+        *{
+            font-family: sans-serif;
+            box-sizing: border-box;
+        }
+
+        form {
+            width: 500px;
+            border: 2px solid #ccc;
+            padding: 30px;
+            background: #fff;
+            border-radius: 15px;
+        }
+
+        h2 {
+            text-align: center;
+            margin-bottom: 40px;
+        }
+
+        input {
+            display: block;
+            border: 2px solid #ccc;
+            width: 95%;
+            padding: 10px;
+            margin: 10px auto;
+            border-radius: 5px;
+        }
+        label {
+            color: #888;
+            font-size: 18px;
+            padding: 10px;
+        }
+
+        button {
+            float: right;
+            background: #555;
+            padding: 10px 15px;
+            color: #fff;
+            border-radius: 5px;
+            margin-right: 10px;
+            border: none;
+        }
+        button:hover{
+            opacity: .7;
+        }
+        .error {
+        background: #F2DEDE;
+        color: #A94442;
+        padding: 10px;
+        width: 95%;
+        border-radius: 5px;
+        margin: 20px auto;
+        }
+
+        h1 {
+            text-align: center;
+            color: #fff;
+        }
+
+        a {
+            float: right;
+            background: #555;
+            padding: 10px 15px;
+            color: #fff;
+            border-radius: 5px;
+            margin-right: 10px;
+            border: none;
+            text-decoration: none;
+        }
+        a:hover{
+            opacity: .7;
+        }
+    </style>
+</head>
+<body>
+     <form action="cek_login.php" method="post">
+     	<h2>LOGIN</h2>
+     	<?php
             if(isset($_GET['pesan'])){
                 if($_GET['pesan'] == "gagal"){
-                    echo "Login gagal! username dan password salah!";
+                    echo "<script>
+                        alert('Login gagal! username dan password salah!!');
+                        window.location='index.php';
+                    </script>";
                 }else if($_GET['pesan'] == "logout"){
-                    echo "Anda telah berhasil logout";
+                    echo "<script>
+                        alert('Anda telah berhasil logout!!');
+                        window.location='index.php';
+                    </script>";
                 }else if($_GET['pesan'] == "belum_login"){
-                    echo "Anda harus login untuk mengakses halaman admin";
+                    echo "<script>
+                        alert('Anda harus login untuk mengakses halaman admin!!');
+                        window.location='index.php';
+                    </script>";
                 }
             }
         ?> 
-        <br/> <br/>
-        <form method="post" action="cek_login.php">
-            <table>
-                <tr>
-                <td>Username</td>
-                <td>:</td>
-                <td>
-                    <input type="text" name="username" placeholder="Masukkan username">
-                </td>
-                </tr>
-                <tr>
-                <td>Password</td>
-                <td>:</td>
-                <td>
-                    <input type="password" name="password" placeholder="Masukkan password">
-                </td>
-                </tr>
-                <tr>
-                <td></td>
-                <td></td>
-                <td>
-                    <input type="submit" value="LOGIN">
-                </td>
-                </tr>
-            </table>
-        </form>
-    </body>
+        <br>
+     	<label>Username</label>
+     	<input type="text" name="username" placeholder="Your Username..."><br>
+
+     	<label>Password</label>
+     	<input type="password" name="password" placeholder="Your Password..."><br>
+
+     	<button type="submit">Login</button>
+     </form>
+</body>
 </html>
